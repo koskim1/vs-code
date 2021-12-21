d94f086351d1eded6fe6d9c8459ceb64b6aed3ec
HyeongJun Kim <koskim1@naver.com>
2021-12-22 01:39:47 +0900

    second commit

--- Hello.c
+++ Hello.c
@@ -4,6 +4,7 @@ int main(void){
     printf("Hello world!.\n");
     printf("is it working?\n");
     printf("ok hanguk mal no?\n");
-    printf("왜 한국말 안됨?\n");
+
+    printf("diffrence?\n");
     return 0;
 }
